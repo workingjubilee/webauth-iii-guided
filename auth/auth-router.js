1837f0cdf9a1f283262c7325fb77ec2ca4762335
workingjubilee <workingjubilee@gmail.com>
2019-04-21 18:56:20 -0700

    End of guided project

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
 
@@ -25,8 +26,11 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +40,17 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const secret = process.env.JWT_SECRET;
+  const options = {
+    expiresIn: '1d',
+  }
+  return jwt.sign(payload, secret, options);  
+}
 
 module.exports = router;
--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,23 @@
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken');
 const Users = require('../users/users-model.js');
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
+  const token = req.headers.authorization;
+  console.log(req.headers);
+  console.log(token);
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (error, decodedToken) => {
+      if (error) {
+        res.status(401).json({ message: 'Invalid credential.' })
+      } else {
+        // req.decodedJwt = decodedToken;
+
+        next();
+      }
+    });
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    res.status(401).json({ message: 'No token provided.' });
   }
 };
